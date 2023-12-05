c41cbe9806677528f45d8e6d87f32bf4fd5c8bb1
sghatak5 <sagnikghatak22@gmail.com>
2023-12-05 12:30:13 +0100

    Loss Function Update

--- Neural Network/CategoricalNN.py
+++ Neural Network/CategoricalNN.py
@@ -35,6 +35,27 @@ class ActivationSoftMax:
         self.output = probabilities
 
 
+class Loss:
+    def calculate(self,output, y):
+        sample_losses = self.forward(output, y)
+        data_loss= np.mean(sample_losses)
+        return data_loss
+
+
+class LossCategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidence = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidence = np.sum(y_pred_clipped*y_true, axis=1)
+
+        neg_log_likelihood = -np.log(correct_confidence)
+        return neg_log_likelihood
+
+
 X, y = create_data(points=100, classes=3)
 
 dense1 = LayerDense(2,3)
@@ -50,4 +71,9 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+loss_function = LossCategoricalCrossEntropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print(loss)
 
